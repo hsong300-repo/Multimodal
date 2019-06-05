02187f817cf12a67f0b16f07e7ec6e987955842f
thdgkdud922@gmail.com <hsong300@gatech.edu>
2019-06-05 15:44:27 -0400

    minor tweaks like default color

--- SVG/keyword_button/textProcess.js
+++ SVG/keyword_button/textProcess.js
@@ -5,7 +5,7 @@ const deleteCommands = ["remove","delete","clear"];
 //shapes
 const shapes = ["circle","rectangle","square","circles","rectangles","squares"];
 //colors
-const colors = ["red","green","yellow","pink","blue","lightblue","purple","gray","grey","white","pink","black","magenta","peru","salmon","wheat","violet","plum","tomato","teal","silver","yellow"]
+const colors = ["red","green","yellow","pink","blue","purple","gray","grey","white","pink","black","magenta","peru","salmon","wheat","violet","plum","tomato","teal","silver","yellow"];
 //counts
 const counts = ["one","two","three","four","five","six","seven","eight","nine","ten"];
 
