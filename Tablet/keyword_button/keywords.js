298041480223ce197c47039975f18cc4dae7d611
thdgkdud922@gmail.com <hsong300@gatech.edu>
2019-06-18 12:50:33 -0400

    blockoverscroll

--- Tablet/keyword_button/keywords.js
+++ Tablet/keyword_button/keywords.js
@@ -94,23 +94,24 @@ document.oncontextmenu = function() {
     return false;
 };
 
-// Add the event listener which gets triggered when using the trackpad
-document.getElementById('container').addEventListener('mousewheel', function(event) {
-    // We don't want to scroll below zero or above the width and height
-    var maxX = this.scrollWidth - this.offsetWidth;
-    var maxY = this.scrollHeight - this.offsetHeight;
+function blockOverscroll ( ) {
 
-    // If this event looks like it will scroll beyond the bounds of the element, prevent it and set the scroll to the boundary manually
-    if (this.scrollLeft + event.deltaX < 0 ||
-        this.scrollLeft + event.deltaX > maxX ||
-        this.scrollTop + event.deltaY < 0 ||
-        this.scrollTop + event.deltaY > maxY) {
+    var pageDirectionX;
 
-        event.preventDefault();
-
-        // Manually set the scroll to the boundary
-        this.scrollLeft = Math.max(0, Math.min(maxX, this.scrollLeft + event.deltaX));
-        this.scrollTop = Math.max(0, Math.min(maxY, this.scrollTop + event.deltaY));
+    function pX(e){
+        return e.originalEvent.changedTouches[0].pageX;
     }
-}, false);
 
+    $(document).on('touchstart', function(e){
+        pageDirectionX = pX(e);
+    });
+
+    $(document).on('touchmove', function(e){
+        if ( pageDirectionX !== pX(e) ) {
+            e.preventDefault();
+            console.log('Stopping overscroll effect');
+        }
+    });
+}
+
+$(blockOverscroll);
