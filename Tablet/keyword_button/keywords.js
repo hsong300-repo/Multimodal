34b3f7f6f15c67e58fb64a4194a8f95e326665d1
thdgkdud922@gmail.com <hsong300@gatech.edu>
2019-06-18 12:37:51 -0400

    hope this works

--- Tablet/keyword_button/keywords.js
+++ Tablet/keyword_button/keywords.js
@@ -94,13 +94,23 @@ document.oncontextmenu = function() {
     return false;
 };
 
-document.body.addEventListener('mousewheel', function(e) {
-    e.stopPropagation();
-    var max = this.scrollWidth - this.offsetWidth; // this might change if you have dynamic content, perhaps some mutation observer will be useful here
+// Add the event listener which gets triggered when using the trackpad
+window.addEventListener('mousewheel', function(event) {
+    // We don't want to scroll below zero or above the width and height
+    var maxX = this.scrollWidth - this.offsetWidth;
+    var maxY = this.scrollHeight - this.offsetHeight;
 
-    if (this.scrollLeft + e.deltaX < 0 || this.scrollLeft + e.deltaX > max) {
-        e.preventDefault();
-        this.scrollLeft = Math.max(0, Math.min(max, this.scrollLeft + e.deltaX));
+    // If this event looks like it will scroll beyond the bounds of the element, prevent it and set the scroll to the boundary manually
+    if (this.scrollLeft + event.deltaX < 0 ||
+        this.scrollLeft + event.deltaX > maxX ||
+        this.scrollTop + event.deltaY < 0 ||
+        this.scrollTop + event.deltaY > maxY) {
+
+        event.preventDefault();
+
+        // Manually set the scroll to the boundary
+        this.scrollLeft = Math.max(0, Math.min(maxX, this.scrollLeft + event.deltaX));
+        this.scrollTop = Math.max(0, Math.min(maxY, this.scrollTop + event.deltaY));
     }
 }, false);
 
