e0bf8de069ec5f2ac9dde46c92486aaca86c97cc
thdgkdud922@gmail.com <hsong300@gatech.edu>
2019-06-18 21:40:30 -0400

    reset

--- Tablet/keywords/keywords.js
+++ Tablet/keywords/keywords.js
@@ -39,10 +39,10 @@ recognition.onresult = function(event) {
                 $("#log").val(final_transcript);
                 document.getElementById('listen').style.display = "none";
             }
-            else{
-                //** this is for test
-                $("#log").val(final_transcript);
-            }
+            // else{
+            //     //** this is for test
+            //     $("#log").val(final_transcript);
+            // }
             // annyang.trigger(final_transcript); //If the sentence is "final" for the Web Speech API, we can try to trigger the sentence
         } else {
             interim_transcript += event.results[i][0].transcript;
