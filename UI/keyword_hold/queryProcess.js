f2c7fd63df5a4eaffde352f14a3a24fe8a9057f6
thdgkdud922@gmail.com <thdgkdud922@gmail.com>
2019-11-05 12:18:01 -0500

    copy commands using make across conditions

--- UI/keyword_hold/queryProcess.js
+++ UI/keyword_hold/queryProcess.js
@@ -106,7 +106,10 @@ function QueryProcess(script){
         console.log('copy');
         copyShapes(n,shape[0],color[0]);
     }else if(makeCommands.filter(n=>tokenStr.indexOf(n)>-1).length>0){
-        if(This_flag === true && color !== "none" && !shape[0]){
+        if(copyCommands.filter(n => tokenStr.indexOf(n) > -1).length > 0) {
+            console.log('copy');
+            copyShapes(n, shape[0], color[0]);
+        }else if(This_flag === true && color !== "none" && !shape[0]){
             // console.log('------change color 1');
             updateShapes(color[0],strokeColor[0]);
         }else if(This_flag === true && color !== "none" && shape[0]){
